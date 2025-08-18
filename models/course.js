209d6fb868b72adf21634246c2a9821da23faaf1
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-08-18 20:02:39 +0530

    latest commit

--- models/course.js
+++ models/course.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const courseSchema = new mongoose.Schema(
+const CourseSchema = new mongoose.Schema(
     {
         title: {
             type: String,
@@ -43,4 +43,39 @@ const courseSchema = new mongoose.Schema(
     }
 )
 
-export default mongoose.model("Course", courseSchema)
+// Static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId },
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageCost: { $avg: "$tuition" },
+            },
+        },
+    ])
+
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// Call getAverageCost after save
+CourseSchema.post("save", function () {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost before deletion
+CourseSchema.pre("save", function () {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+CourseSchema
+
+export default mongoose.model("Course", CourseSchema)
